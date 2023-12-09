d891e186d2e704218fd917b4433692788b7f1fe3
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-12-09 22:40:08 +0530

    feat: add item model

---
+++ models/item.model.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const itemSchema = new mongoose.Schema({
+  item: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+});
+
+const Item = mongoose.model("Item", itemSchema);
+
+module.exports = { Item, itemSchema };
