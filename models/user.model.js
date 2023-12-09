9be5209f260cc4717053bf2bf39d8cf7380ce29a
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-12-09 22:40:15 +0530

    feat: add user model

---
+++ models/user.model.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const cartSchema = new mongoose.Schema({
+  item: { type: String, required: true },
+  price: { type: Number, required: true },
+  quantity: { type: Number, required: true },
+});
+
+const orderSchema = new mongoose.Schema({
+  order: [cartSchema],
+  discountEligible: Boolean,
+  discountCode: String,
+  totalAmount: Number,
+  totalItems: Number,
+  totalDiscountAmount: Number,
+});
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  cart: [cartSchema],
+  orders: [orderSchema],
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
