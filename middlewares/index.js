9f5ad77aa52852bef2a346344731d189536933af
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-12-10 12:52:55 +0530

    remove files

--- middlewares/index.js
+++
@@ -1,9 +0,0 @@
-const authVerify = require("./authVerify.middleware");
-const errorHandler = require("./errorHandler.middleware");
-const routeNotFound = require("./routeNotFound.middleware");
-
-module.exports = {
-  authVerify,
-  errorHandler,
-  routeNotFound,
-};
--- utils/utils.js
+++
@@ -1,8 +0,0 @@
-const jwt = require("jsonwebtoken");
-const JWT_SECRET = process.env.JWT_SECRET;
-
-const generateToken = (userId) => {
-  return jwt.sign({ userId }, JWT_SECRET, { expiresIn: "24h" });
-};
-
-module.exports = { generateToken };
